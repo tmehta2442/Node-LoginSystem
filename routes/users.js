164663ab14ca45c47b94c01b730934c482b414c8
tmehta2442 <tmehta@post.harvard.edu>
2016-08-07 20:34:43 -0700

    Mongo db, models and user registration

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({dest: './uploads'});
+var upload = multer({dest: '../uploads/'});
+
+var User = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,9 +17,10 @@ router.get('/register', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login', {title:'Login'});
 });
+//router.post('/register',upload.single('profileimage'),function(req, res, next)
 
 //
-router.post('/register', upload.single('profileimage'),  function(req, res, next) {
+router.post('/register',upload.single('profileimage'),function(req, res, next) {
   var name = req.body.name;
   var email = req.body.email;
   var username = req.body.username;
@@ -49,7 +52,23 @@ router.post('/register', upload.single('profileimage'),  function(req, res, next
   		errors: errors
   	});
   } else {
-  	console.log('No Errors');
+  	var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileimage: profileimage
+    });
+
+    User.createUser(newUser, function(err, user){
+      if(err) throw err;
+      console.log(user);
+    });
+
+    req.flash('success', 'You are now registered and can now login');
+    
+    res.location('/');
+    res.redirect('/');
   }
 
 });
